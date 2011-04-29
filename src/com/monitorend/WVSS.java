ee7ea26ed9c99f3210ff7c0dc6181d48908117a3
sld19891005 <sld19891005@yeah.net>
2011-04-29 12:40:18 +0800

    add the command dialog.

--- src/com/monitorend/WVSS.java
+++ src/com/monitorend/WVSS.java
@@ -5,14 +5,19 @@ import java.io.InputStream;
 import java.net.Socket;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
 import android.net.wifi.WifiManager.MulticastLock;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class WVSS extends Activity {
 
@@ -20,6 +25,7 @@ public class WVSS extends Activity {
 	private ReceiveNaluThread mReceiveNaluThread;
 //	private MulticastLock mMulticastLock;
 	private DatagramPacketQueue mDatagramPacketQueue;
+	private Dialog mCommandDialog;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -65,24 +71,67 @@ public class WVSS extends Activity {
 	}
 
 	// Menu item IDs
-    public static final int CONNECTING_ID = Menu.FIRST;    
-    public static final int EXIT_ID = Menu.FIRST + 1; 
+    public static final int SENDCOMMAND_ID = Menu.FIRST;    
+    public static final int EXIT_ID = Menu.FIRST + 1;
+	private static final int SEND_COMMAND_DIALOG = 1;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         
-        menu.add(0, CONNECTING_ID, 0, R.string.connect_server);   
+        menu.add(0, SENDCOMMAND_ID, 0, R.string.sendCommand);   
         menu.add(0, EXIT_ID, 1, R.string.exit);
 
         return true;
     }
 
     @Override
+	protected Dialog onCreateDialog(int id) {
+		
+    	switch(id) 
+    	{
+    	case SEND_COMMAND_DIALOG:
+    		return (initCommandDialog());
+    	}
+		return super.onCreateDialog(id);
+	}
+    
+    private Dialog initCommandDialog() {
+    	
+    	AlertDialog.Builder builder = new AlertDialog.Builder(WVSS.this);
+    	LayoutInflater factory = LayoutInflater.from(this);
+    	final View commandDialogView = factory.inflate(R.layout.send_command, null);
+    	
+    	builder.setIcon(R.drawable.icon);
+    	builder.setTitle(R.string.CommandTitle);
+    	builder.setView(commandDialogView);
+    	builder.setPositiveButton(R.string.command_ok, new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+    	
+    	builder.setNegativeButton(R.string.command_cancle, new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+    	
+    	return mCommandDialog;
+    }
+
+	@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {        
-	        case CONNECTING_ID:
+	        case SENDCOMMAND_ID:
 	        {	
+	        	showDialog(SEND_COMMAND_DIALOG);
 //	        	new ConnectCenterServerThread(mWvssView).start();
 	        	
 	            return true;
